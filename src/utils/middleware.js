f7eba7aa6e7ff9f35458d4ff958631c9a45240c6
relative <relative@riseup.net>
2020-03-31 22:17:28 -0400

    Update middleware.js

--- src/utils/middleware.js
+++ src/utils/middleware.js
@@ -9,7 +9,7 @@ module.exports = async (req, res, next) => {
     return res.redirect('/oauth/login')
   }
   const user = await getUser(req.session.token)
-  if (!user.admin) {
+  if (!user.admin || !req.moderator) {
     delete req.session.token
     req.flash(
       'is-danger',
